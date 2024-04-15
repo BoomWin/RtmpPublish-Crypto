8cc84c2bfab39373a1f3feaf9d81316c955aab52
Boomwin <20192223@kookmin.ac.kr>
2024-04-15 20:14:19 +0900

    Push All

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+val libraryGroup by rootProject.extra { "com.github.pedroSG94" }
+val vCode by rootProject.extra { 244 }
+val vName by rootProject.extra { "2.4.4" }
+val coroutinesVersion by rootProject.extra { "1.7.3" }
+val junitVersion by rootProject.extra { "4.13.2" }
+val mockitoVersion by rootProject.extra { "5.2.1" }
+
+plugins {
+  id("com.android.application") version "8.3.1" apply false
+  id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+  id("org.jetbrains.dokka") version "1.9.20" apply true
+}
+
+tasks.register("clean") {
+  delete(layout.buildDirectory)
+}
+
+tasks.dokkaHtmlMultiModule.configure {
+  outputDirectory.set(File("docs"))
+}
